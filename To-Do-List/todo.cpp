43ad837656b08f47c59af55feec7cd035ef657d4
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-05-19 10:25:45 +0330

    todo updated

--- To-Do-List/todo.cpp
+++ To-Do-List/todo.cpp
@@ -12,3 +12,13 @@ void addTask() {
     tasks.push_back(task);
     cout << "Task added!\n";
 }
+void showTasks() {
+    if (tasks.empty()) {
+        cout << "No tasks yet!\n";
+        return;
+    }
+    cout << "----- Your Tasks -----\n";
+    for (int i = 0; i < tasks.size(); i++) {
+        cout << i + 1 << ". " << tasks[i] << endl;
+    }
+}
