f1a965ad7783a7acdb45c67dbfef2de6bcd9bc3b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-05-19 10:09:02 +0330

    update: vector<string> tasks;

--- To-Do-List/todo.cpp
+++ To-Do-List/todo.cpp
@@ -2,3 +2,13 @@
 #include <vector>
 #include <fstream>
 using namespace std;
+vector<string> tasks;
+
+void addTask() {
+    string task;
+    cout << "Enter a new task: ";
+    cin.ignore(); // برای جلوگیری از خطای دریافت ورودی
+    getline(cin, task);
+    tasks.push_back(task);
+    cout << "Task added!\n";
+}
