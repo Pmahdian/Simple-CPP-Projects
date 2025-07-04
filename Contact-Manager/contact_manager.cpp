8f52d9fa7f8e40c5b054927e9e490ba0bb6ece84
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-04 14:04:54 +0330

    updating

--- Contact-Manager/contact_manager.cpp
+++ Contact-Manager/contact_manager.cpp
@@ -9,3 +9,43 @@ struct Contact {
 };
 
 vector<Contact> contacts;
+
+void loadContacts() {
+    ifstream file("contacts.txt");
+    if (file.is_open()) {
+        Contact c;
+        while (file >> c.name >> c.phone) {
+            contacts.push_back(c);
+        }
+        file.close();
+    }
+}
+void saveContacts() {
+    ofstream file("contacts.txt");
+    for (const auto& c : contacts) {
+        file << c.name << " " << c.phone << endl;
+    }
+    file.close();
+}
+
+void addContact() {
+    Contact c;
+    cout << "Enter name: ";
+    cin >> c.name;
+    cout << "Enter phone: ";
+    cin >> c.phone;
+    contacts.push_back(c);
+    saveContacts();
+    cout << "Contact added!\n";
+}
+
+void viewContacts() {
+    if (contacts.empty()) {
+        cout << "No contacts found!\n";
+        return;
+    }
+    cout << "----- Contacts -----\n";
+    for (int i = 0; i < contacts.size(); i++) {
+        cout << i+1 << ". " << contacts[i].name << " - " << contacts[i].phone << endl;
+    }
+}
